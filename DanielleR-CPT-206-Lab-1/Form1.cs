7bee4f33aea8863b8ed9e17b743297278a094125
Danielle Rodriguez <140760987+DanielleRCodes@users.noreply.github.com>
2024-01-22 15:39:24 -0500

    Add files via upload

---
+++ DanielleR-CPT-206-Lab-1/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DanielleR_CPT_206_Lab_1
+{
+    public partial class Form1 : Form
+    {
+        //Global Variables that store user input from textboxes on Form1
+        int userUnits = 0;
+        decimal userPriceMin = 0.0m;
+        decimal userPriceMax = 0.0m;
+        string userProductNum = "";
+        string userDescription = "";
+
+        ResultsForm formResults = new ResultsForm();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void productBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.productDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'productDBDataSet.Product' table. You can move, or remove it, as needed.
+            this.productTableAdapter.Fill(this.productDBDataSet.Product);
+
+        }
+
+        private void btnProdNum_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Catchs missing user input
+                if (txtProductNumber.Text == "")
+                {
+                    MessageBox.Show("Please Enter a Product Number");
+                }
+                else
+                {
+                    userProductNum = txtProductNumber.Text;
+
+                    ProductDataDataContext db = new ProductDataDataContext();
+
+                    var results = from Product in db.Products
+                                  where Product.Product_Number == userProductNum
+                                  select Product.Description;
+                    formResults.lBoxResults.Items.Clear();
+
+                    foreach (string description in results)
+                    {
+                        formResults.lBoxResults.Items.Add(description);
+                    }
+
+                    formResults.Show();
+                }
+            }catch { MessageBox.Show("Error!"); }
+        }
+
+        private void btnDescripText_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtDescriptionText.Text == "")
+                {
+                    MessageBox.Show("Please Enter a Product Name");
+                }
+                else
+                {
+                    userDescription = txtDescriptionText.Text;
+
+                    ProductDataDataContext db = new ProductDataDataContext();
+
+                    var results = from Product in db.Products
+                                  where Product.Description == userDescription
+                                  select Product.Description;
+                    formResults.lBoxResults.Items.Clear();
+
+                    foreach (string description in results)
+                    {
+                        formResults.lBoxResults.Items.Add(description);
+                    }
+
+                    formResults.Show();
+                }
+            }catch { MessageBox.Show("Error!"); }
+        }
+
+        private void btnSortByUnitsASC_Click(object sender, EventArgs e)
+        {
+            ProductDataDataContext db = new ProductDataDataContext();
+
+            var results = from product in db.Products
+                          orderby product.Units_On_Hand
+                          select product.Description;
+
+            formResults.lBoxResults.Items.Clear();
+
+            foreach (string description in results)
+            {
+                formResults.lBoxResults.Items.Add(description);
+            }
+
+            formResults.Show();
+        }
+
+        private void btnMoreThanUnits_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                userUnits = int.Parse(txtUnitAmount.Text);
+
+                if (txtUnitAmount.Text == "")
+                {
+                    MessageBox.Show("Please Enter Correct Units");
+                }
+                else
+                {
+                    ProductDataDataContext db = new ProductDataDataContext();
+
+                    var results = from product in db.Products
+                                  where product.Units_On_Hand > userUnits
+                                  select product.Description;
+
+                    formResults.lBoxResults.Items.Clear();
+
+                    foreach (string description in results)
+                    {
+                        formResults.lBoxResults.Items.Add(description);
+                    }
+
+                    formResults.Show();
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void btnLessThanUnits_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                userUnits = int.Parse(txtUnitAmount.Text);
+
+                if (txtUnitAmount.Text == "")
+                {
+                    MessageBox.Show("Please Enter Correct Units");
+                }
+                else
+                {
+                    ProductDataDataContext db = new ProductDataDataContext();
+
+                    var results = from product in db.Products
+                                  where product.Units_On_Hand < userUnits
+                                  select product.Description;
+
+                    formResults.lBoxResults.Items.Clear();
+
+                    foreach (string description in results)
+                    {
+                        formResults.lBoxResults.Items.Add(description);
+                    }
+
+                    formResults.Show();
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void btnSortByPriceASC_Click(object sender, EventArgs e)
+        {
+            ProductDataDataContext db = new ProductDataDataContext();
+
+            var results = from product in db.Products
+                          orderby product.Price
+                          select product.Description;
+
+            formResults.lBoxResults.Items.Clear();
+
+            foreach (string description in results)
+            {
+                formResults.lBoxResults.Items.Add(description);
+            }
+
+            formResults.Show();
+        }
+
+        private void btnFindInRange_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                userPriceMin = decimal.Parse(txtPriceMin.Text);
+                userPriceMax = decimal.Parse(txtPriceMax.Text);
+
+                ProductDataDataContext db = new ProductDataDataContext();
+
+                var results = from product in db.Products
+                              where product.Price > userPriceMin
+                              where product.Price < userPriceMax
+                              select product.Description;
+
+                formResults.lBoxResults.Items.Clear();
+
+                foreach (string description in results)
+                {
+                    formResults.lBoxResults.Items.Add(description);
+                }
+
+                formResults.Show();
+
+            }catch { MessageBox.Show("Error"); }
+        }
+    }
+}
---
+++ DanielleR-CPT-206-Lab-1/ResultsForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace DanielleR_CPT_206_Lab_1
+{
+    partial class ResultsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lBoxResults = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Here are your results!";
+            // 
+            // lBoxResults
+            // 
+            this.lBoxResults.FormattingEnabled = true;
+            this.lBoxResults.ItemHeight = 20;
+            this.lBoxResults.Location = new System.Drawing.Point(28, 65);
+            this.lBoxResults.Name = "lBoxResults";
+            this.lBoxResults.Size = new System.Drawing.Size(366, 204);
+            this.lBoxResults.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(153, 322);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(143, 60);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Close";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // ResultsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(425, 437);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lBoxResults);
+            this.Controls.Add(this.label1);
+            this.Name = "ResultsForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ResultsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        public System.Windows.Forms.ListBox lBoxResults;
+    }
+}
